2d98c07397b0ba2011a9cb2943026625488c849e
Benjamin Ranson <benr444@bellsouth.net>
2021-06-07 13:14:13 -0400

    initialize repository.

---
+++ ROptics backup 1.jl
@@ -0,0 +1,60 @@
+module ROptics
+using Revise
+using Images, FileIO, Plots, FFTW
+import Base.real
+
+# ----- OpticField -----
+struct OpticParams
+	λ::Float64
+end
+export OpticParams
+OpticParams() = OpticParams(1.0)
+
+struct OpticField # the minimum data for an optic-design
+	values::Matrix{Complex{Float64}} # 2D Array of Complex numbers
+	params::OpticParams
+	#scale::Float64 # physical-distance between matrix slots
+	#λ::Float64 # physical wavelength
+end
+export OpticField
+OpticField(depths::Matrix{Complex{Float64}}; params = OpticParams()) = OpticField(depths, params)
+
+function OpticField(filename; params = OpticParams())
+	image = load(filename)
+	gray_array = grayModulus.(image)
+	OpticField(gray_array, params)
+end
+
+shape(x::OpticField) = shape(x.values)
+F(x::OpticField) = OpticField(fft(x.values); x.params) # forward-propagate
+export F
+F⁻¹(x::OpticField) = OpticField(ifft(x.values), x.params) # reverse-propagate
+export F⁻¹
+
+Plots.png(x::OpticField, filepath) = png(plot(Gray24.(fitMyRange(real(x.values)))), filepath)
+# ^ method extension to plot
+#export png
+
+set_phase(z, Φ) = complex(abs(z) * cos(Φ), abs(z) * sin(Φ))
+set_phase(x::OpticField, Φs) = OpticField(broadcast(set_phase, x.values, Φs), x.params)
+export set_phase
+set_modulus(z, l) = complex(abs(l) * cos(angle(z)), abs(l) * sin(angle(z)))
+set_modulus(x::OpticField, Φs) = OpticField(broadcast(set_modulus, x.values, Φs), x.params)
+export set_modulus
+
+# ----- END OpticField -----
+
+function grayModulus(c::AbstractRGBA)
+	colors = (red(c), green(c), blue(c))
+	(sqrt ∘ sum)((colors.^2))
+end
+export grayModulus
+
+function fitMyRange(xx)
+	xx_min = minimum(xx)
+	range = maximum(xx) - xx_min
+	(xx .- minimum)./range # -1 -> 1
+end
+export fitMyRange
+
+end
---
+++ test.jl
@@ -0,0 +1,37 @@
+include("ROptics.jl")
+using Revise
+using Main.ROptics
+using Images, FileIO, Plots, FFTW
+
+function design_optic()
+
+my_λ = 1.0 # units:
+my_scale = 1.0 # units:
+g, gˈ, G, Gˈ = Dict(), Dict(), Dict(), Dict() # prime marker is \verts
+
+G[0] = OpticField("in/three.png")
+g[0] = F⁻¹(G[0])
+png(g[0], "out/three-g0.png")
+total_cycles = 3
+for i in 1:total_cycles
+	gˈ[i] = set_phase(g[i - 1], 0)
+	G[i] = F(gˈ[i])
+	Gˈ[i] = set_modulus(G[i], G[0].values)
+	g[i] = F⁻¹(Gˈ[i])
+	println("Cycle $i finished.")
+	#r_Gˈ = (abs.(G[i].values))
+	png(heatmap(
+		heatmap(abs.(gˈ[i].values), title="|gˈ|"),
+		heatmap(abs.(G[i].values), title="|G|"),
+		heatmap(abs.(Gˈ[i].values), title="|Gˈ|"),
+		heatmap(abs.(g[i].values), title="|g|"),
+		layout=4, plot_title="absolute values"), "out/three-moduli$i.png")
+	png(heatmap(
+		heatmap(angle.(gˈ[i].values), title="Φ(gˈ)"),
+		heatmap(angle.(G[i].values), title="Φ(G)"),
+		heatmap(angle.(Gˈ[i].values), title="Φ(Gˈ)"),
+		heatmap(angle.(g[i].values), title="Φ(g)"),
+		layout=4, plot_title="phase"), "out/three-phases$i.png")
+end
+
+end # end design_optic
