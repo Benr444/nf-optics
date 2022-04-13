c368a7ae89b7abe34c208e949760a0e68d85fd99
Benjamin Ranson <benr444@bellsouth.net>
2022-04-13 16:31:23 -0400

    adds svg sources; errors as norms

--- DesignKinoform.jl
+++ DesignKinoform.jl
@@ -10,12 +10,12 @@
 module DesignKinoform
 
 using Revise
-using Images, FileIO, FFTW
+using Images, FileIO, FFTW, Random
 using Plots; pyplot()
 Revise.includet("ROptics.jl")
 using Main.ROptics
 
-file_name = "shape-1-200px"
+file_name = "uofsc-200px"
 
 Plots.default(aspect_ratio = :auto, size = (800, 800))
 
@@ -35,6 +35,9 @@ G_errors = zeros(total_cycles)
 G[0] = Complex{Float64}.(make_field("in/$(file_name).png")) # read in pattern. cast to Complex64 type.
 #G[0] = set_phase(G[0], 0)
 g[0] = F⁻¹(G[0]) # create relief guess by reverse-propagating pattern
+random_phases = (rand(Float64, size(g[0])) .- 0.5) .* 2π
+# ^ generate random phases. start with r ∈ (0 to 1]. then rescale to fit in (-π to π].
+g[0] = set_phase(g[0], random_phases)
 png(heatmap(abs.(G[0]), title="|G|"), "out/$(file_name)--moduli-G0.png")
 png(heatmap(abs.(half_roll(g[0])), title="|g|"), "out/$(file_name)--moduli-gg0.png")
 png(heatmap(angle.(G[0]), title="Φ(G)"), "out/$(file_name)--phases-G0.png")
@@ -47,24 +50,25 @@ for i in 1:total_cycles
 	g[i] = F⁻¹(Gˈ[i])
 
 	#@show gˈ_modulus_mean = mean(abs.(gˈ[i]))
-	#@show gˈmodulus_diff = round(sum_sq_diff(abs.(gˈ[i]), 1.0), sigdigits = 3)
+	#@show gˈmodulus_diff = round(norm(abs.(gˈ[i]), 1.0), sigdigits = 3)
 	#@show gˈ_modulus_is_1 = all(abs.(gˈ[i]) .== 1.0)
 
 	#@show sizes_match = (size(g[i]) == size(G[i]))
 
 	G_raw = F(g[i - 1]) # uncorrected propagation.
-	@show correction_diff = round(sum_sq_diff(abs.(G_raw), abs.(G[i])), sigdigits = 3)
+	@show correction_diff = round(norm(abs.(G_raw), abs.(G[i])), sigdigits = 3)
 
-	@show insanity_modulus_diff = round(sum_sq_diff(abs.(G_raw), abs.(G[0])), sigdigits = 3)
-	@show insanity_phase_diff = round(sum_sq_diff(angle.(G_raw), angle.(G[0])), sigdigits = 3)
+	@show insanity_modulus_diff = round(norm(abs.(G_raw), abs.(G[0])), sigdigits = 3)
+	@show insanity_phase_diff = round(norm(angle.(G_raw), angle.(G[0])), sigdigits = 3)
 	
-	G_errors[i] = sum_sq_diff(abs.(G[0]), abs.(G[i]))
-	@show round(G_errors[i], sigdigits = 3)
-	@show Gˈ_error = round(sum_sq_diff(abs.(G[0]), abs.(Gˈ[i])), sigdigits = 3)
-	@show g_error = round(sum_sq_diff(abs.(g[0]), abs.(g[i])), sigdigits = 3)
-	@show gˈ_error = round(sum_sq_diff(abs.(g[0]), abs.(gˈ[i])), sigdigits = 3)
+	G_errors[i] = norm(abs.(G[0]), abs.(G[i]))
+	#@show round(G_errors[i], sigdigits = 3)
+	println("G_errors[$i] = ", G_errors[i])
+	@show Gˈ_error = round(norm(abs.(G[0]), abs.(Gˈ[i])), sigdigits = 3)
+	@show g_error = round(norm(abs.(g[0]), abs.(g[i])), sigdigits = 3)
+	@show gˈ_error = round(norm(abs.(g[0]), abs.(gˈ[i])), sigdigits = 3)
 
-	@show g_change = round(sum_sq_diff(abs.(g[i - 1]), abs.(g[i])), sigdigits = 3)
+	@show g_change = round(norm(abs.(g[i - 1]), abs.(g[i])), sigdigits = 3)
 
 	png(heatmap(abs.(G[i]), title="|G|"), "out/$(file_name)--moduli-G$i.png")
 	png(heatmap(abs.(half_roll(g[i])), title="|g|"), "out/$(file_name)--moduli-gg$(i).png")
@@ -81,7 +85,7 @@ end
 
 
 save("out/$(file_name)--moduli-G-final-matrix.png", cap(abs.(G[total_cycles])))
-#G_errors_step = (maximum(G_errors) - minimum(G_errors)) / length(G_errors)
+G_errors_step = (maximum(G_errors) - minimum(G_errors)) / 10#length(G_errors)
 G_errors_plot = plot(G_errors,
 					 title = "Errors of G",
 					 label = "",
@@ -89,7 +93,7 @@ G_errors_plot = plot(G_errors,
 					 xlabel = "Cycle",
 					 ylabel = "Sum Square Differences of Moduli of G relative to G[0]",
 					 xlims = (1, length(G_errors)),
-					 series_annotations = string.(round.(G_errors, sigdigits = 3)),
+					 #series_annotations = string.(round.(G_errors, sigdigits = 3)),
 					 ylims = (minimum(G_errors), maximum(G_errors)),
 					 yticks = (minimum(G_errors):G_errors_step:maximum(G_errors)))
 
