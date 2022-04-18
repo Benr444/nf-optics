a2889023e2632d5e1ba19d18e0081d300d074968
Benjamin Ranson <benr444@bellsouth.net>
2022-04-18 16:38:06 -0400

    changes \verts to \prime

--- DesignKinoform.jl
+++ DesignKinoform.jl
@@ -15,7 +15,7 @@ using Plots; gr()
 Revise.includet("ROptics.jl")
 using Main.ROptics
 
-file_name = "uofsc-200px"
+file_name = "sad-200px"
 illumination_file_name = "illumination-200px"
 
 Plots.default(aspect_ratio = :auto, size = (1200, 1200), fontfamily = font("Computer Modern"))
@@ -25,13 +25,13 @@ my_scale = 1.0 # units:
 low_freq_filter_size = lffs = 20
 total_cycles = 20
 
-g, gˈ, G, Gˈ = Dict(), Dict(), Dict(), Dict() # prime marker is \verts
+g, g′, G, G′ = Dict(), Dict(), Dict(), Dict() # prime marker is \prime
 # g = relief guess.
-# gˈ = phase-zero'd relief guess (same moduli as g, since that is unaffected).
+# g′ = phase-zero'd relief guess (same moduli as g, since that is unaffected).
 # G = far-field pattern.
-# Gˈ = moduli-corrected pattern (same moduli as given initial pattern).
+# G′ = moduli-corrected pattern (same moduli as given initial pattern).
 
-h, hˈ, H, Hˈ = Dict(), Dict(), Dict(), Dict()
+h, h′, H, H′ = Dict(), Dict(), Dict(), Dict()
 # binary-level data
 
 G_errors = zeros(total_cycles)
@@ -41,65 +41,68 @@ H_errors = zeros(total_cycles)
 I = ComplexF64.(make_field("in/$illumination_file_name.png"))
 #save("out/$file_name--I.png", cap(abs.(I)))
 
-Gˈ[0] = Complex{Float64}.(make_field("in/$(file_name).png")) # read in pattern. cast to Complex64 type.
-image_size = size(Gˈ[0])[1]
-random_phases = (rand(Float64, size(Gˈ[0])) .- 0.5) .* 2π
+G′[0] = Complex{Float64}.(make_field("in/$(file_name).png")) # read in pattern. cast to Complex64 type.
+image_size = size(G′[0])[1]
+random_phases = (rand(Float64, size(G′[0])) .- 0.5) .* 2π
 # ^ generate random phases. start with r ∈ (0 to 1]. then rescale to fit in (-π to π].
-gˈ[0] = F⁻¹(Gˈ[0])# create relief guess by reverse-propagating pattern.
-gˈ[0] = set_phase(gˈ[0], random_phases)
-g[0] = set_modulus(gˈ[0], 1) # force-satisfy constraints for first object guess.
+g′[0] = F⁻¹(G′[0])# create relief guess by reverse-propagating pattern.
+g′[0] = set_phase(g′[0], random_phases)
+g[0] = set_modulus(g′[0], 1) # force-satisfy constraints for first object guess.
 
-Hˈ[0] = copy(Gˈ[0])
-hˈ[0] = copy(gˈ[0])
+H′[0] = copy(G′[0])
+h′[0] = copy(g′[0])
 h[0] = copy(g[0])
 
 β = 0.5
-function fienup_fix(zˈ, z)
-	if abs(zˈ) > 0
-		return zˈ
+function fienup_fix(z′, z)
+	if abs(z′) > 0
+		return z′
 	else
-		return z - (β * zˈ)
+		return z - (β * z′)
 	end
 end
 
-function scale_fix(zˈ, z)
+function scale_fix(z′, z)
 	# abs(z) is 1, assumed, bc it satisfies object constraints.
-	if abs(zˈ) < 1
-		return zˈ + β * z
-	elseif abs(zˈ) > 1
-		return zˈ - β * z
+	if abs(z′) < 1
+		return z′ + β * z
+	elseif abs(z′) > 1
+		return z′ - β * z
 	else
-		return zˈ
+		return z′
 	end
 end
 
 for i in 1:total_cycles
-	#g[i] = broadcast(fienup_fix, gˈ[i - 1], g[i - 1])
-	g[i] = scale_fix.(gˈ[i - 1], g[i - 1])
-	#g[i] = set_modulus(gˈ[i - 1], gˈ[0])
-	#g[i] = set_modulus(gˈ[i - 1], 1)
+	#g[i] = broadcast(fienup_fix, g′[i - 1], g[i - 1])
+	#g[i] = scale_fix.(g′[i - 1], g[i - 1])
+	#g[i] = set_modulus(g′[i - 1], g′[0])
+	g[i] = set_modulus(g′[i - 1], 1)
+	#g[i] = set_modulus(g′[i - 1], I)′
+	abs.(g[i])
 	G[i] = F(g[i])
-	Gˈ[i] = set_modulus(G[i], Gˈ[0])
-	gˈ[i] = F⁻¹(Gˈ[i])
+	G′[i] = set_modulus(G[i], G′[0])
+	g′[i] = F⁻¹(G′[i])
 	
-	h[i] = set_modulus(hˈ[i - 1], hˈ[0])
+	h[i] = set_modulus(h′[i - 1], h′[0])
 	H[i] = F(h[i])
-	Hˈ[i] = set_modulus(H[i], Hˈ[0]) # set the modulus of the current guess to be that of the target
-	hˈ[i] = F⁻¹(Hˈ[i])
+	H′[i] = set_modulus(H[i], H′[0]) # set the modulus of the current guess to be that of the target
+	h′[i] = F⁻¹(H′[i])
 
-	#G_errors[i] = f_mse(abs.(G[i]), abs.(Gˈ[0]))
-	G_errors[i] = f_mse(abs.(G[i]), abs.(Gˈ[i]))
-	#G_errors[i] = norm(abs.(G[i]), abs.(Gˈ[0]))
+	#G_errors[i] = f_mse(abs.(G[i]), abs.(G′[0]))
+	G_errors[i] = f_mse(abs.(G[i]), abs.(G′[i]))
+	#G_errors[i] = norm(abs.(G[i]), abs.(G′[0]))
 	println("G_errors[$i] = ", □(G_errors[i]))
-	#H_errors[i] = f_mse(abs.(H[i]), abs.(Hˈ[0]))
-	H_errors[i] = f_mse(H[i], Hˈ[i])
-	#H_errors[i] = norm(abs.(H[i]), abs.(Hˈ[0]))
+	#H_errors[i] = f_mse(abs.(H[i]), abs.(H′[0]))
+	H_errors[i] = f_mse(H[i], H′[i])
+	#H_errors[i] = norm(abs.(H[i]), abs.(H′[0]))
 	#println("H_errors[$i] = ", □(H_errors[i]))
 
-	#png(heatmap(abs.(Gˈ[i]), title="|G′|"), "out/$(file_name)--moduli-G'$i.png")
-	png(heatmap(abs.(half_roll(gˈ[i])), title="|g′|"), "out/$(file_name)--moduli-gg'$(i).png")
-	#png(heatmap(angle.(Gˈ[i]), title="Φ(G′)"), "out/$(file_name)--phases-G'$i.png")
-	png(heatmap(angle.(half_roll(gˈ[i])), title="Φ(g′)"), "out/$(file_name)--phases-gg'$(i).png")
+	#png(heatmap(abs.(G′[i]), title="|G′|"), "out/$(file_name)--moduli-G'$i.png")
+	png(heatmap(abs.(half_roll(g′[i])), title="|g′|"), "out/$(file_name)--moduli-gg'$(i).png")
+	png(heatmap(abs.(half_roll(g[i])), title="|g|"), "out/$(file_name)--moduli-gg$(i).png")
+	#png(heatmap(angle.(G′[i]), title="Φ(G′)"), "out/$(file_name)--phases-G'$i.png")
+	png(heatmap(angle.(half_roll(g′[i])), title="Φ(g′)"), "out/$(file_name)--phases-gg'$(i).png")
 
 	#png(heatmap(abs.(G[i]), title="|G|"), "out/$(file_name)--moduli-G$i.png")
 	#png(heatmap(abs.(half_roll(g[i])), title="|g|"), "out/$(file_name)--moduli-gg$(i).png")
@@ -112,9 +115,9 @@ for i in 1:total_cycles
 	println("Cycle $i finished.")
 end
 
-gˈ★ = gˈ[total_cycles] # \bigstar
+g′★ = g′[total_cycles] # \bigstar
 g★ = g[total_cycles] # \bigstar
-save("out/$file_name--moduli-gg'-final.png", half_roll(cap(abs.(gˈ★))))
+save("out/$file_name--moduli-gg'-final.png", half_roll(cap(abs.(g′★))))
 save("out/$file_name--moduli-G-final.png", cap(abs.(F(g★))))
 save("out/$file_name--phases-gg-final.png", half_roll(cap(angle.(g★))))
 
@@ -142,11 +145,11 @@ errors_plot = plot!(errors_plot,
 				 	yticks = ■(3).(minimum(G_errors):G_errors_step:maximum(G_errors)),) 
 png(errors_plot, "out/$file_name--errors.png")
 
-# filter off the high-frequency values. IMPORTANT: do not roll gˈ★[i] before doing this!
-#gˈ_final_filtered = gˈ★[1:lffs, 1:lffs]
-#Gˈ_final_filtered = F(vcat(hcat(gˈ_final_filtered, zeros(lffs, image_size - lffs)), zeros(image_size - lffs, image_size)))
-#save("out/$file_name--G'-final--low-freq-$lffs.png", cap(abs.(Gˈ_final_filtered)))
-#save("out/$file_name--g'-final--low-freq-$lffs.png", cap(abs.(gˈ_final_filtered)))
+# filter off the high-frequency values. IMPORTANT: do not roll g′★[i] before doing this!
+#g′_final_filtered = g′★[1:lffs, 1:lffs]
+#G′_final_filtered = F(vcat(hcat(g′_final_filtered, zeros(lffs, image_size - lffs)), zeros(image_size - lffs, image_size)))
+#save("out/$file_name--G'-final--low-freq-$lffs.png", cap(abs.(G′_final_filtered)))
+#save("out/$file_name--g'-final--low-freq-$lffs.png", cap(abs.(g′_final_filtered)))
 
 #png(heatmap(I, title="I"), "out/$(file_name)--I.png")
 q = half_roll(g★) # basically q at least
